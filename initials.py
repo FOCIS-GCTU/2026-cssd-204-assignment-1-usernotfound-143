68c1d363eba6ad3cf4858e91a186099b8f7e4b9c
usernotfound-143 <2425401048@live.gctu.edu.gh>
2026-05-30 18:58:57 +0000

    #commit initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Ernest Akyea-Mensah
+# STUDENT ID: 2425401048
+# Email: 2425401048@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Ernest Akyea-Mensah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+  # Print initials E, A,M in small then large stylized block letters
 
+  print()
+  print("...EAM")
+  print()
+  print("...EEEEEEEEEEEE    .......AAAA....    ...MM........MM    ...")
+  print("...EE..........    ......AA..AA...    ...MMM......MMM    ...")
+  print("...EE..........    .....AA....AA..    ...MMMM....MMMM    ...")
+  print("...EEEEEEEE....    ....AA......AA.    ...MM.MM..MM.MM    ...")
+  print("...EEEEEEEE....    ...AA........AA    ...MM..MMMM..MM    ...")
+  print("...EE..........    ...AAAAAAAAAAAA    ...MM...MM...MM    ...")
+  print("...EE..........    ...AA........AA    ...MM........MM    ...")
+  print("...EE..........    ...AA........AA    ...MM........MM    ...")
+  print("...EE..........****...AA........AA****...MM........MM****...")
+  print("...EEEEEEEEEEEE****...AA........AA****...MM........MM****...")
+  print()
 
 
 main()
